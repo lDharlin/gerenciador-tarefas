68b106818e3edc3e8a1d30176520107634e814cc
Luan Dharlin <luandharlinls@gmail.com>
2024-08-31 18:31:59 -0300

    first commit

---
+++ src/main/java/com/gerenciador/tarefas/DemogerenciadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciador.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogerenciadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogerenciadorTarefasApplication.class, args);
+	}
+
+}
